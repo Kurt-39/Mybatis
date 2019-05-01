c5778027e5ee189433632bf2b8c9547a17c54569
kurt <yumao852314815@163.com>
2019-05-01 22:45:42 +0800

    #mybatis第二天

--- src/main/java/com/kurt/mybatis/dao/MybatisFirst.java
+++ src/main/java/com/kurt/mybatis/dao/MybatisFirst.java
@@ -10,6 +10,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 
 public class MybatisFirst {
@@ -41,8 +44,64 @@ public class MybatisFirst {
                 sqlSession.close();
             }
         }
+    }
+    @Test
+    public void testFindUserByUsername(){
+        SqlSession sqlSession = getSqlSession();
+        List<Object> list = sqlSession.selectList("test.findUserByUsername", "四");
+        System.out.println(list);
+        sqlSession.close();
+    }
 
+    @Test
+    public void testInsertUser() throws Exception{
+        SqlSession sqlSession = getSqlSession();
+        User user = new User();
+        user.setUsername("BOB");
+        user.setAddress("TIANMEN");
+        String birthday = "1993-7-4";
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(birthday);
+        user.setBirthday(date);
+        user.setSex("1");
+        int insert = sqlSession.insert("test.insertUser", user);
+        System.out.println(user.getId());
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    //delete
+    @Test
+    public void deleteUser(){
+        SqlSession sqlSession = getSqlSession();
+        sqlSession.delete("test.deleteUser",40);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    //update
+    @Test
+    public void updateUser() throws Exception{
+        SqlSession sqlSession = getSqlSession();
+        User user = new User();
+        user.setUsername("JIM");
+        user.setAddress("TIANMEN");
+        String birthday = "1993-7-4";
+        Date date = new SimpleDateFormat("yyyy-MM-dd").parse(birthday);
+        user.setBirthday(date);
+        user.setSex("1");
+        user.setId(1);
+        sqlSession.update("test.updateUser",user);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    //创建sqlSession
+    private SqlSession getSqlSession() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = sqlSessionFactory.openSession();
 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sqlSession;
     }
 
 }
--- src/main/java/com/kurt/mybatis/pojo/User.java
+++ src/main/java/com/kurt/mybatis/pojo/User.java
@@ -1,10 +1,14 @@
 package com.kurt.mybatis.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     private int id;
     private String username;
